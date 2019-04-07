d52f5575082deb648c530d62d6e4882cd735077c
Andrew J. Pounds <pounds_aj@mercer.edu>
2019-04-07 15:44:30 -0400

    working on new method to zero in on solution

--- fwevfun.py
+++ fwevfun.py
@@ -5,8 +5,7 @@ V = 1.6e-18      # volts
 m = 9.11e-31     # kilograms
 hbar = 1.0546e-34
 
-alpha = 0.02
-delta = 0.0000000001
+alpha = 0.029
 h = 1.0e-14
 u = 0.0
 root=0
@@ -19,6 +18,7 @@ while (alpha < 0.03):
    x = 0.0
    oldu = 0.0 
    u = 0.0
+   delta = 0.0001
 
 # Based on the odd/even nature of solution, set initial psi and dpsi
    if ( evenfunction ):
@@ -51,11 +51,50 @@ while (alpha < 0.03):
    else:
 # if the oldpsi and the new psi are different, we found a solution
        if ( (psi * oldpsi) < 0.0 ):
-           print "*** FOUND   SOLUTION  ***,  alpha = ", str(alpha)
+           print ("*** FOUND   SOLUTION  ***,  alpha =  %18.16f", alpha)
            # Now put the wavefunction data in a file
            root = root + 1
            filename = "wavefunction_" + str(root) + ".dat"
            filehandle = open(filename,"w")
+
+          # improve the solution
+           iteration = 0
+           oldpsi = -psi
+           while (iteration < 10):
+             oldalpha = alpha
+             alpha = alpha - delta 
+             delta = delta / 10             
+             print ("==== current alpha is %18.16f", alpha)
+             solutionFound = False
+             while ( not solutionFound  ):
+                alpha = alpha + delta;
+                if ( evenfunction ):
+                  psi = 1.0
+                  dpsi = 0.0
+                else:
+                  psi = 0.0
+                  dpsi = 1.0
+
+# The next loop propagates the solution of the funtion to u=2 
+                u = 0.0
+                while ( u < 2.0 ):
+                   x = x + h
+                   u = x / width
+                   du = u - oldu
+                   if ( abs(u) > 0.5 ):
+                     c = 2 * m * width**2 * V * (1.0 - alpha) / hbar**2
+                   else:
+                     c = - 2 * m * width**2 * V * alpha / hbar**2
+           
+                   dpsi = dpsi + c * psi * du
+                   psi = psi + dpsi * du
+                   oldu = u
+
+                if ((psi * oldpsi) < 0 ):
+                   solutionFound = True
+                   iteration = iteration + 1 
+                   print( "improved alpha is %18.16f on iteration %i"  % (alpha, iteration)) 
+
            if ( evenfunction ):
               psi = 1.0
               dpsi = 0.0
@@ -89,7 +128,7 @@ while (alpha < 0.03):
            firstrun = True
            evenfunction = not evenfunction
    
-#   print "Checking alpha = " + str(alpha)        
+   print "Checking alpha = " + str(alpha)        
    alpha = alpha + delta
 
 
